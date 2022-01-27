6cc3e8d612bb7d0a4223912bb1de54d34f52c136
zyakdi <yakdi.zak@gmail.com>
2022-01-27 18:39:10 +0100

    Add contract deployment script

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,22 +1,24 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider')
-const Web3 = require('web3')
-const compiledFactory = require('./build/CampaignFactory.json')
-require('dotenv').config()
-
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
 const provider = new HDWalletProvider(
-    process.env.METAMASK_PHRASE,
-    process.env.INFURA_LINK // infura enables us to connect to a real node on the network
-)
-const web3 = new Web3(provider)
+  'MY_METAMASK_PHRASE',
+  'https://rinkeby.infura.io/v3/MY_INFURA_ID'
+);
+const web3 = new Web3(provider);
 
 const deploy = async () => {
-    const accounts = await web3.eth.getAccounts()
-    console.log('Attempting to deploy from account', accounts[0]);
-    const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
-        .deploy({data: compiledFactory.bytecode})
-        .send({gas:'1000000', from:accounts[0]});
+  const accounts = await web3.eth.getAccounts();
 
-    console.log('Contract deployed to ', result.options.address);
-    provider.engine.stop()
-}
-deploy()
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
--- pages/campaigns/[address]/requests/index.js
+++ pages/campaigns/[address]/requests/index.js
@@ -64,7 +64,7 @@ export async function getServerSideProps({ query }) {
     );
     return { 
         props: {
-            address, requests, requestCount, approversCount 
+            address, requests: JSON.parse(JSON.stringify(requests)), requestCount, approversCount 
         }
     }
 }
